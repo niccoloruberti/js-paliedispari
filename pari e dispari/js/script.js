8084c24e0987bb28b2c14bc7925526fb590a5649
niccoloruberti <nikiruberti@gmail.com>
2023-05-25 18:18:57 +0200

    esercizio pari e dispari finito

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,1 +1,48 @@
-'use stric';
+'use strict';
+
+//creo una funzione che generi un numero random per il computer
+function numberRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//creo una funzione che controlla se un numero è pari o dispari
+function oddEven(number) {
+    if (number % 2 === 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
+//dichiaro le variabili
+let numberUser;
+let numberComputer;
+let choseUser;
+let bottone = document.getElementById('play');
+let somma;
+let result;
+
+//creo un evento per quando viene premuto il bottone
+bottone.addEventListener('click', function() {
+    //prendo i dati inseriti dall'utente
+    numberUser = parseInt(document.getElementById('numberUser').value);
+    console.log(numberUser);
+    console.log(typeof(numberUser));
+    choseUser = document.getElementById('choseUser').value;
+    //genero un numero casuale per il computer
+    numberComputer = numberRandom(1, 5);
+    console.log(numberComputer);
+    //faccio la somma dei due numeri
+    somma = numberUser + numberComputer;
+    console.log(somma);
+    //controllo se il numero ottenuto dalla somma è pari o dispari
+    result = oddEven(somma);
+    console.log(result);
+    //mostro il risultato a schermo
+    if (result === choseUser) {
+        alert('Hai vinto!');
+    } else {
+        alert('Mi dispiace, hai perso!');
+    }
+})
+
